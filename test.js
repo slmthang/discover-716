ec53225e3e659c56829ddbbbe9cea4f8ba33996d
thangs2399 <47937323+thangs2399@users.noreply.github.com>
2024-04-04 01:40:26 -0400

    added intro message, about us, slideshow
    need to work on slideshow

---
+++ test.js
@@ -0,0 +1,35 @@
+
+
+
+let showIndex = 1;
+displaySlides(showIndex);
+
+
+let nextButton = document.getElementById("next");
+let prevButton = document.getElementById("prev");
+
+nextButton.onclick = () => calShowIndex(1);
+prevButton.onclick = () => calShowIndex(-1);
+
+
+function calShowIndex(n) {
+    displaySlides(showIndex += n);
+}
+
+
+function displaySlides(n) {
+
+    let slides = document.querySelectorAll("#slides .slide");
+
+    if (n > slides.length) {
+        showIndex = 1;
+    } else if (n < 1) {
+        showIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[showIndex-1].style.display = "block";
+}
