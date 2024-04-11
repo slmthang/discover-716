c6c82b332c2527eeb0c434980c24c4268f202b80
thangs2024 <slmthang2024@gmail.com>
2024-04-11 06:53:57 -0400

    fixed navs in footer, made some changes for responsiveness of the website

--- js/scripts.js
+++ js/scripts.js
@@ -28,8 +28,8 @@ setInterval(changeIntro, 4500);
 /******************** About Us Pictures Slider ********************/
 
 const checkbound = function (n) {
-    if (n > 13) {
-        return n % 13;
+    if (n > 9) {
+        return n % 9;
     } else {
         return n;
     }
@@ -40,7 +40,7 @@ function getRandomInt(max) {
 }
 
 const genNums = function () {
-    let num = getRandomInt(12);
+    let num = getRandomInt(9);
     return [num, checkbound(num + 1) , checkbound(num + 2), checkbound(num + 3) , checkbound(num + 4), checkbound(num + 5)];
 }
 
@@ -50,7 +50,7 @@ const thumbsMobile = document.querySelectorAll("#thumbs-mobile .thumbs");
 const picsSlider = function () {
     const pics = genNums();
 
-    if (window.innerWidth > 800) {
+    if (window.innerWidth > 767) {
         for (let i = 0; i < thumbs.length; i++) {
             thumbs[i].setAttribute("src", `./img/homepage/thumbs/${pics[i]}.jpeg`);
         }
@@ -72,41 +72,38 @@ const shrinkTopNav = function () {
     const navBar1 = document.getElementById("top-nav-bar1");
         const navBar2 = document.querySelector("#top-nav-bar2");
         const navBar2_img = document.querySelector("#top-nav-bar2 .logo img");
-        const navBar2_ddNavMenu = document.querySelector("#top-nav-bar2 #dropdown-nav-menu");
-
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         
-        // navBar1
-        navBar1.style.height = "2rem"; 
+    if (window.innerWidth > 767) { 
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            
+            // navBar1
+            navBar1.style.height = "2rem"; 
 
-        // navBar2
-        navBar2.style.height = "6rem";
+            // navBar2
+            navBar2.style.height = "6rem";
 
-        // navBar2_img
-        navBar2_img.style.width = "60%";
-        navBar2_img.style.height = "60%";
+            // navBar2_img
+            navBar2_img.style.width = "60%";
+            navBar2_img.style.height = "60%";
 
-        // navBar2_ddNavMenu
-        navBar2_ddNavMenu.style.height = "2rem";
 
-    } else {
-        // navBar1
-        navBar1.style.height = "3rem"; 
+        } else {
+            // navBar1
+            navBar1.style.height = "3rem"; 
 
-        // navBar2
-        navBar2.style.height = "10rem";
+            // navBar2
+            navBar2.style.height = "10rem";
 
-        // navBar2_img
-        navBar2_img.style.width = "1000%";
-        navBar2_img.style.height = "1000%";
+            // navBar2_img
+            navBar2_img.style.width = "100%";
+            navBar2_img.style.height = "100%";
 
-        // navBar2_ddNavMenu
-        navBar2_ddNavMenu.style.height = "3rem";
+        }
     }
 }
 
 // shrinkTopNav()
-// window.onscroll = () => shrinkTopNav();
+window.onscroll = () => shrinkTopNav();
 
 /************************************************************/
 
